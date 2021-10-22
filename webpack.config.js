08fbf7d41ca1c610c0edf9f9e92c6f80dbf0a92a
Svetlana-Kutyeva1974 <81332075+Svetlana-Kutyeva1974@users.noreply.github.com>
2021-10-22 23:30:41 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'production',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: './app.bundle.js',
+    filename: 'app.bundle.js',
   },
   module: {
     rules: [
