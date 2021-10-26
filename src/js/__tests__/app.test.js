405499267e4b9c87d0b31f7fe3e9db0b435975f2
Svetlana Kutyova <kutyovas@yandex.ru>
2021-10-26 22:28:58 +0300

    3

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,8 +1,11 @@
 import person from '../app.js';
 
-test('should person', () => {
-  const result = person({ name: 'Маг', health: 90 });
-  expect(result).toBe('healthy');
+test('person return value live level : healthy', () => {
+  expect(person({ name: 'Маг', health: 90 })).toBe('healthy');
+});
+test('person return value live level :wounded', () => {
   expect(person({ name: 'Маг', health: 45 })).toBe('wounded');
+});
+test('person return value live level : critical', () => {
   expect(person({ name: 'Лучник', health: 10 })).toBe('critical');
 });
